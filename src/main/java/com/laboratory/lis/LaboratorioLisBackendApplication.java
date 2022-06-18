bf42ca541fe0eb709763e709921339eda634fc1c
DiegoMurcia2022 <diegomurciac2220@gmail.com>
2022-06-17 22:56:56 -0500

    initialized project

---
+++ src/main/java/com/laboratory/lis/LaboratorioLisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laboratory.lis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioLisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioLisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laboratory/lis/LaboratorioLisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratory.lis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioLisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
